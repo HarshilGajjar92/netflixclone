95028b2cbac95de88207481236647a00af5eda7e
HarshilGajjar92 <gajjar.harshil1992@gmail.com>
2022-12-12 19:20:29 +0530

    Configured the SpringApplication

--- src/main/java/com/HarshilGajjar92/Application.java
+++ src/main/java/com/HarshilGajjar92/Application.java
@@ -1,7 +1,11 @@
 package com.HarshilGajjar92;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world from Java+Gradle");
+        SpringApplication.run(Application.class, args);
     }
 }
